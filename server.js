10775fab9be00c40f93517d74110b90f0440bc8b
Senthamarai27 <93567658+Senthamarai27@users.noreply.github.com>
2023-11-28 22:39:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
-const connectDb = require("./config/dbConnection");
-const bodyParser = require("body-parser")
-const expressBodyParser = require("express")
-connectDb();
-const app = express()
-const port = 5000;
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+const connectDb = require('./config/dbConnection');
+const bodyParser = require('body-parser');
 
-// parse application/json
-app.use(bodyParser.json())
-// View Engine Setup
+const app = express();
+const port = 3000;
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:false}));
+
+//parse application/json
+app.use(bodyParser.json());
+
+//View engine setup
 app.use(express.static(__dirname + '/public'));
-app.use(expressLayouts)
-app.set('layout', './layouts/main')
+app.use(expressLayouts);
+app.set('layout','./layouts/main');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use('/',require('./routes/projectRouter'));
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-app.use("/",require("./routes/projectRouter"))
-
+//Creating server
 app.listen(port,(req,res)=>{
-    console.log("Server running at", port)
+    console.log("Server running at ",port);
+    connectDb();
 })
